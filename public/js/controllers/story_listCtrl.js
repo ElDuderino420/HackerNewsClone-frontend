940193117a4fdf4f054c864baf49cf124d59ceae
Marco Blum <marco8jnv@hotmail.com>
2017-10-10 14:26:30 +0200

    restart server and login stuffs

--- public/js/controllers/story_listCtrl.js
+++ public/js/controllers/story_listCtrl.js
@@ -1,4 +1,26 @@
 angular.module('haxorNews')
 .controller('story_listCtrl', function($scope) {
+    $scope.message = 'Motherfucking search page.';
+    $scope.availableFag = {};
+    $scope.availableFag.temp = [
+        {name:'mathematics', level:''},
+        {name:'danish', level:''},
+        {name:'physics',level:''},
+        {name:'chemistry',level:''},
+        {name:'english',level:''},
+        {name:'history',level:''},
+        {name:'geography',level:''},
+        {name:'religion',level:''},
+        {name:'social science',level:''},
+        {name:'biology',level:''},
+        {name:'music',level:''},
+        {name:'statistics',level:''}];
+    $scope.availableFag.grade = ['4th','5th','6th','7th','8th','9th','10th','11th','12th','13th',];
+    $http.get('/api/search').then(function(res){
+        $scope.list = res.data;
 
+        $scope.resetSearch = function () {
+            $scope.filterstuff = {};
+        }
+    });
 });
--- public/js/controllers/user/loginCtrl.js
+++ public/js/controllers/user/loginCtrl.js
@@ -1,4 +1,22 @@
 angular.module('haxorNews')
 .controller('loginCtrl', function($scope) {
 
-});
+    var urlParams = $location.search();
+    $scope.alertText = urlParams.msg;
+    $scope.user = {
+        email: '',
+        password: ''
+    };
+
+    $scope.login = function() {
+        AuthService.login($scope.user).then(function(user) {
+            $location.path('/profile/'+user.displayName);
+            $scope.setCurrentUser(user);
+            $scope.isAuthorized = AuthService.isAuthorized;
+            console.log("login success");
+        }, function(errMsg) {
+
+            $scope.dangerText = errMsg
+        });
+    };
+})
